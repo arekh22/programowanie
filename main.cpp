eb0af3ed993bf2453a347f6caec5b006baebefeb
arekh22 <arekh332@gmail.com>
2021-10-27 14:06:36 +0200

    funkcjaliniowa(1.4)

--- main.cpp
+++ main.cpp
@@ -1,10 +1,11 @@
-//wersja 1.2
+//wersja 1.4
 #include <iostream>
 
 using namespace std;
 
 int main(int argc, char** argv) {
-	float a, b;
+	float a, b, A, B, C;
+	cout<<"funkcja liniowa w postaci y=ax+b "<<endl;
 	cout<<"podaj a: ", cin>>a;
 	cout<<"podaj b: ", cin>>b;
 	float x;
@@ -17,7 +18,23 @@ int main(int argc, char** argv) {
 		x=-b/a;
 		cout<<"miejsce zerowe funkcji: "<<x<<endl;
 	}
-
+	
+	cout<<"\nfunkcja liniowa w postaci kanonicznej"<<endl;
+	
+	cout<<"podaj A: ", cin>>A;
+	cout<<"podaj B: ", cin>>B;
+	cout<<"podaj C: ", cin>>C;
+	
+	float y;
+	if(A==0)
+	{
+		cout<<"funkcja stala"<<endl;
+	}
+	else
+	{
+		x=-C/A;
+		cout<<"miejsce zerowe funkcji: "<<x<<endl;
+	}
 	
 	
 	return 0;
