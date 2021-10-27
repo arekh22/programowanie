a62a6437b5f3c807aa1f8adcba807206595ce1bb
arekh22 <arekh332@gmail.com>
2021-10-27 13:37:05 +0200

    Pliki startowe

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char** argv) {
+	float a, b;
+	cout<<"podaj a: ", cin>>a;
+	cout<<"podaj b: ", cin>>b;
+	float x;
+	x=-b/a;
+	cout<<"miejsce zerowe funkcji: "<<x<<endl;
+	
+	
+	
+	return 0;
+}
